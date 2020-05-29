bae395b98c815533ed27b9d5c3b2d74715a05748
Dong Liu <35363823+Fluidy@users.noreply.github.com>
2020-05-29 23:27:00 +0100

    Add files via upload

---
+++ Syn_preference.m
@@ -0,0 +1,70 @@
+function [ q ] = Syn_preference( p, s, theta )
+Nf = length(p);
+Nu = length(s);
+q = zeros(Nu, Nf);
+pp = p;
+pp0 = pp;
+
+availabe_user = 1:Nu;
+for i_ue = 1 : Nu
+    ii = randperm(length(availabe_user), 1);
+    ue = availabe_user(ii);
+    availabe_user(ii) = [];
+
+%     ue = i_ue;
+
+    
+    rho = min(pp/s(ue), 1);
+    g = zeros(1, Nf);
+    aa = 1;
+    availabe_file = 1:Nf;
+%     theta = betarnd(a,b);
+    while aa > 1e-8 && ~isempty(availabe_file)
+        ff = randperm(length(availabe_file), 1);
+        f = availabe_file(ff);
+        
+        scale = (aa/sum(pp0(availabe_file))).^theta; 
+        
+%         x = min(beta*pp0(f)*scale, rho(f));
+%         y = min((beta*pp0(f) + (1-beta)*rho(f))*scale, rho(f));
+%         rand_var = rand;
+%         temp = min(aa, x + (y-x)*rand_var);
+
+        x = theta*pp0(f);
+        y = theta*pp0(f) + (1-theta)*rho(f);       
+        temp = min(min(aa, scale*(x + (y-x)*rand)), rho(f));
+        
+        g(f) = temp;
+        
+        aa = aa - g(f);
+        availabe_file(ff) = [];
+
+    end
+
+    if aa > 1e-8 % && isempty(availabe_file)
+        not_full_file = find(rho - g > 0);
+        while ~isempty(not_full_file) && aa > 1e-8
+            ff = randperm(length(not_full_file), 1);
+            f = not_full_file(ff);
+            temp = min(g(f) + aa, rho(f));
+            aa = aa - (temp - g(f));
+            g(f) = temp;
+            if g(f) == rho(f)
+                not_full_file(ff) = [];
+            end
+        end
+    end
+
+    q(ue,:) = g;
+    pp = max( pp - s(ue).*q(ue,:), 0);
+    
+    if min(pp) < 0
+        warning('!!');
+    end
+    
+    pp0 = pp/sum(pp);
+end
+
+for ue = 1 : Nu
+    q(ue, :) = q(ue, :)*s(ue);
+end
